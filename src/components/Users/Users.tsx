8112a27247aa25b0f845726c107c7da147abcc12
Senka Petrovic <petrovicsenka@outlook.com>
2025-03-11 21:29:24 +0100

    Improved table and pagination with Ant Design

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { useTypedSelector } from "../../hooks/useTypedSelector";
 import { fetchUsers, selectUsers } from "../../redux/user/userSlice";
 import { AppDispatch } from "../../redux/store";
+import { Table, Pagination } from "antd";
 import "./Users.module.scss";
 
 const Users = () => {
@@ -10,75 +11,73 @@ const Users = () => {
   const users = useTypedSelector(selectUsers);
 
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 20;
+  const [itemsPerPage, setItemsPerPage] = useState(20);
 
   useEffect(() => {
     dispatch(fetchUsers());
   }, [dispatch]);
 
-  const totalPages = Math.ceil(users.length / itemsPerPage);
-
   const indexOfLastUser = currentPage * itemsPerPage;
   const indexOfFirstUser = indexOfLastUser - itemsPerPage;
   const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser);
+
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
+      title: "First Name",
+      dataIndex: "first_name",
+      key: "first_name",
+    },
+    {
+      title: "Last Name",
+      dataIndex: "last_name",
+      key: "last_name",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Gender",
+      dataIndex: "gender",
+      key: "gender",
+    },
+    {
+      title: "IP Address",
+      dataIndex: "ip_address",
+      key: "ip_address",
+    },
+  ];
 
   return (
     <div className="styledWrapper">
       <h1>User List</h1>
 
-      <table className="table">
-        <thead>
-          <tr>
-            <th className="th">ID</th>
-            <th className="th">First Name</th>
-            <th className="th">Last Name</th>
-            <th className="th">Email</th>
-            <th className="th">Gender</th>
-            <th className="th">IP Address</th>
-          </tr>
-        </thead>
-        <tbody>
-          {currentUsers.map((user) => (
-            <tr key={user.id}>
-              <td className="td">{user.id}</td>
-              <td className="td">{user.first_name}</td>
-              <td className="td">{user.last_name}</td>
-              <td className="td">{user.email}</td>
-              <td className="td">{user.gender}</td>
-              <td className="td">{user.ip_address}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-
-      <div className="pagination">
-        <button onClick={() => setCurrentPage(1)} disabled={currentPage === 1}>
-          First
-        </button>
-        <button
-          onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
-          disabled={currentPage === 1}
-        >
-          Prev
-        </button>
-        <span>
-          Page {currentPage} of {totalPages}
-        </span>
-        <button
-          onClick={() =>
-            setCurrentPage((prev) => Math.min(prev + 1, totalPages))
-          }
-          disabled={currentPage === totalPages}
-        >
-          Next
-        </button>
-        <button
-          onClick={() => setCurrentPage(totalPages)}
-          disabled={currentPage === totalPages}
-        >
-          Last
-        </button>
+      <div className="table">
+        <Table
+          columns={columns}
+          dataSource={currentUsers}
+          pagination={false}
+          rowKey="id"
+        />
       </div>
+
+      <Pagination
+        current={currentPage}
+        total={users.length}
+        pageSize={itemsPerPage}
+        onChange={setCurrentPage}
+        onShowSizeChange={(current, size) => setItemsPerPage(size)}
+        showSizeChanger={true}
+        showQuickJumper
+        responsive
+        style={{ marginTop: "20px", display: "flex", justifyContent: "center" }}
+      />
     </div>
   );
 };
